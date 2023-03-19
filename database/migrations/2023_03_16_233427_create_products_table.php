882b50ad36897c8db7b54acfa201ab8ee8c64010
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-03-20 01:57:25 +0700

    update nullable on column description

--- database/migrations/2023_03_16_233427_create_products_table.php
+++ database/migrations/2023_03_16_233427_create_products_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('price');
             $table->integer('sum');
-            $table->longText('description');
+            $table->longText('description')->nullable();
             $table->timestamps();
         });
     }
