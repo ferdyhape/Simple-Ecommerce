0c8f8fc900d2f6f0829b2c3ce121530ea8f6f942
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-03-20 05:04:06 +0700

    update dropdown animation

--- resources/views/layouts/user_side/navbar.blade.php
+++ resources/views/layouts/user_side/navbar.blade.php
@@ -14,12 +14,12 @@
                 <li class="nav-item">
                     <a class="nav-link {{ Request::is('cart') ? 'active' : '' }}" href="{{ url('cart') }}">Cart</a>
                 </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown ">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         {{ auth()->user()->name }}
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul class="dropdown-menu dropdown-menu-end animate slideIn">
                         @if (auth()->user()->role_id == '1')
                             <li>
                                 <a href="{{ url('/dashboard/category') }}" class="dropdown-item">Dashboard</a>
@@ -39,3 +39,52 @@
         </div>
     </div>
 </nav>
+<style>
+    @media (min-width: 768px) {
+        .animate {
+            animation-duration: 0.3s;
+            -webkit-animation-duration: 0.3s;
+            animation-fill-mode: both;
+            -webkit-animation-fill-mode: both;
+        }
+    }
+
+    @keyframes slideIn {
+        0% {
+            transform: translateY(1rem);
+            opacity: 0;
+        }
+
+        100% {
+            transform: translateY(0rem);
+            opacity: 1;
+        }
+
+        0% {
+            transform: translateY(1rem);
+            opacity: 0;
+        }
+    }
+
+    @-webkit-keyframes slideIn {
+        0% {
+            -webkit-transform: transform;
+            -webkit-opacity: 0;
+        }
+
+        100% {
+            -webkit-transform: translateY(0);
+            -webkit-opacity: 1;
+        }
+
+        0% {
+            -webkit-transform: translateY(1rem);
+            -webkit-opacity: 0;
+        }
+    }
+
+    .slideIn {
+        -webkit-animation-name: slideIn;
+        animation-name: slideIn;
+    }
+</style>
